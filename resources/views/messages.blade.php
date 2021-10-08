03cd1013968ee0f168405d43980d147d11965c15
Vadim Shablii <mefistovad@gmail.com>
2021-10-08 21:29:38 +0300

    refactoring part3.4

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,12 +1,16 @@
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+        @foreach($errors->all() as $error)
+            <li> {{ $error }} </li>
+        @endforeach
+        </ul>
+    </div>
+@endif
+
+
 @if(Session::has('flash_message'))
     <div class="alert alert-{{ session('flash_message')['class'] }}">
         {{ session('flash_message')['message'] }}
     </div>
 @endif
-
-@if(Session::has('errors'))
-    <div class="alert alert-danger">
-        {{ session('errors') }}
-    </div>
-@endif
-
